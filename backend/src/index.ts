2c846227b41e0bfb8e80ad482d1f253296b299b5
ansh <kumaransh1975@gmail.com>
2024-07-09 10:27:00 +0530

    improved file structure

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,21 @@
-import express,{Request,Response} from "express"
+import express, { Request, Response } from "express";
 import cors from "cors";
-import dbconnect from "./dbconnect";
+import dbconnect from "./utils/dbconnect";
 
-const app = express()
+const app = express();
 const port = 3000;
 
-dbconnect()
+dbconnect();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get("/",function(req,res){
-    res.json({
-        msg: "hello world!"
-    })
-})
+app.get("/", function (req, res) {
+  res.json({
+    msg: "hello world!",
+  });
+});
 
-app.listen(port,()=>{
-    console.log(`server listening on ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server listening on ${port}`);
+});
