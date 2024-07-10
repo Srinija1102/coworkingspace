b01ca82616330e343f27572039ed207c29875193
krish858 <unknowmortal57@gmail.com>
2024-07-10 23:11:53 +0530

    used middlewares

--- backend/src/routes/AuthRoutes.ts
+++ backend/src/routes/AuthRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { protect } from "../middlewares/authMiddleware";
 import { login,logout } from "../controllers/AuthControllers";
 
 const router = Router()
 
-router.post("/login",login);
+router.post("/login",protect,login);
 router.post("/logout",logout);
 
 export default router
