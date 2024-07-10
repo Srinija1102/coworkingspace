cf0db92f27561a3aa4f64f2ec0cc19cc59e4aa1e
learnershakil <learnershakil@gmail.com>
2024-07-10 21:56:27 +0530

    created booking route

---
+++ backend/src/routes/booking.route.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { protect } from "../middleware/authMiddleware";
+import {
+  createBooking,
+  getAllBookings,
+  getBookingById,
+  getBookingsByUserId,
+  updateBookingStatus,
+  deleteBooking
+} from "../controllers/booking.controller";
+
+const router = Router();
+
+router.post("/", protect, createBooking);
+router.get("/", protect, getAllBookings);
+router.get("/:id", protect, getBookingById);
+router.get("/user/:id", protect, getBookingsByUserId);
+router.put("/:id", protect, updateBookingStatus);
+router.delete("/:id", protect, deleteBooking);
+
+export default router;
