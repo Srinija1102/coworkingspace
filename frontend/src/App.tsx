dfd227d4745e2ed4b056d716d0140a4dada6c5d6
ansh <kumaransh1975@gmail.com>
2024-07-12 21:03:16 +0530

    aa

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Offer from "./components/service/offer";
 import Explore from "./components/service/explore";
+import OurServicesPage from "./pages/Service";
 function App() {
   return (
     <div className="flex">
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Landing />} />
-      </Routes> */}
-      <Offer
+        <Route path="/service" element={<OurServicesPage />} />
+      </Routes>
+      {/* <Offer
         title="Flexible Workspaces"
         description="Our state-of-the-art coworking spaces are crafted to cater to your specific needs. Whether you're a solopreneur, a startup team, or a remote worker, we have the perfect workspace for you"
         imagesrc="https://i0.wp.com/www.603thecoworkingspace.com/wp-content/uploads/2023/08/icon1.png?fit=1123%2C962&ssl=1"
@@ -17,7 +19,7 @@ function App() {
         title="Flexible Workspaces"
         description="Our state-of-the-art coworking spaces are crafted to cater to your specific needs. Whether you're a solopreneur, a startup team, or a remote worker, we have the perfect workspace for you"
         imagesrc="https://i0.wp.com/www.603thecoworkingspace.com/wp-content/uploads/2023/08/icon1.png?fit=1123%2C962&ssl=1"
-      />
+      /> */}
     </div>
   );
 }
---
+++ frontend/src/pages/Service.tsx
@@ -0,0 +1,11 @@
+import Explore from "../components/service/explore";
+import Offer from "../components/service/offer";
+const OurServicesPage: React.FC = () => {
+  return (
+    <div className="container mt-44 mx-auto p-4 border-2 h-full border-gray-100">
+      <div></div>
+    </div>
+  );
+};
+
+export default OurServicesPage;
